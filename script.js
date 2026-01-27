fccfecff0068b37e37ba3b3d20e463b617a65947
Bryan <bryanwagoki2016@gmail.com>
2026-01-27 19:23:58 +0300

    feat: update the switch to use a function and be fully accessible with mouse and keyboard navigation

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let toggler = document.querySelector(".toggle");
 
 
 toggler.addEventListener("keydown",(e)=>{
-  if(e.key !== "Enter" || e.key !== " "){
+  if(e.key !== "Enter" && e.key !== " "){
     return
 }
-
-
-
+e.preventDefault()
+let currentState = toggler.getAttribute("aria-checked");
+console.log(currentState)
 })
