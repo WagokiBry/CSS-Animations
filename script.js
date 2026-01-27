6e04b0bc2d337b24dbe5dab7395e3a0c298d5517
Bryan <bryanwagoki2016@gmail.com>
2026-01-27 19:25:49 +0300

    refactor: fetch from remote to update code base

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let toggler = document.querySelector(".toggle");
 
+toggler.focus()
 
-toggler.addEventListener("keydown",(e)=>{
-  if(e.key !== "Enter" && e.key !== " "){
+function toggleSwitch(e){
+   if(e.key !== "Enter" && e.key !== " " && e.type !== "click"){
     return
 }
 e.preventDefault()
-let currentState = toggler.getAttribute("aria-checked");
-console.log(currentState)
-})
+let currentState = toggler.getAttribute("aria-checked")==="true";
+let isOn = !currentState;
+
+toggler.textContent = isOn? "On" : "Off"
+toggler.setAttribute("aria-checked", String(isOn));
+}
+
+toggler.addEventListener("keydown", toggleSwitch);
+toggler.addEventListener("click", toggleSwitch);
