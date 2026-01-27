c5c9782899053b9eda64a7b499714807131a86b1
Bryan <bryanwagoki2016@gmail.com>
2026-01-27 16:34:00 +0300

    feat: focus on the div

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 
-const unhide  = document.querySelector(".unhide")
-unhide.addEventListener("click",()=>{
-  const menu = document.getElementById('hiddenMenu');
-  menu.style.display = 'block';
-menu.querySelector('button').focus();
-})
+const toggler  = document.querySelector(".toggle")
+toggler.focus()
+ 
 
